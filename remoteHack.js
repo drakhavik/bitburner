309abada8d8a1cd41978458e2ee45b5ab8c521a8
drakhavik <drakhavik@users.noreply.github.com>
2022-09-21 11:52:12 -0400

    Create remoteHack.js
    getHostname() not getHostName(), and computerName isn't defined, that was a variable from another script.

--- remoteHack.js
+++ remoteHack.js
@@ -5,6 +5,6 @@ export async function main(ns) {
 	while(true) {
 		if (ns.getServerSecurityLevel(host) > 5) {await ns.weaken(host)}
 		if (ns.getServerMoneyAvailable(host) < 1000) {await ns.grow(host)}
-		else await ns.hack(computerName)
+		else await ns.hack(host)
 	}
 }
