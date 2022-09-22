7aa2f832f03778964ab7f3ba9daf01ebcc9c6da1
drakhavik <drakhavik@users.noreply.github.com>
2022-09-22 13:31:38 -0400

    Update remoteHack.js
    change to min 20 sec level

--- remoteHack.js
+++ remoteHack.js
@@ -3,7 +3,7 @@ export async function main(ns) {
     const host = ns.getHostname()
     /** @param {NS} ns */
 	while(true) {
-		if (ns.getServerSecurityLevel(host) > 5) {await ns.weaken(host)}
+		if (ns.getServerSecurityLevel(host) > 20) {await ns.weaken(host)}
 		if (ns.getServerMoneyAvailable(host) < 1000) {await ns.grow(host)}
 		else await ns.hack(host)
 	}
